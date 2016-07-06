26d98915d774352bb18eadd74e95686720fcea8e
Tim Simpson <timsimpson4@gmail.com>
2016-07-06 00:52:43 -0500

    Changes build system to Conan, adds AppVeyor

--- project.lua
+++ project.lua
@@ -1,18 +1,29 @@
-import("Macaroni", "ProjectTemplates", "1")
+local Path = require "Macaroni.IO.Path"
+local cmfw = require "Macaroni.Generators.CMake.CMakeFileWriter"
 
 
-require "SimpleProject"
+project = context:Group("BorderTown"):Project("Solace"):Version("XYZ")
+lib = project:Library{
+    name="lib",
+    headers=pathList{"src", "target"},
+    sources=pathList{"src"},
+}
 
-SimpleProject{
-  group="BorderTown",
-  project="Solace",
-  version="1.0.0.0",
-  src="src",
-  target="target",
-  dependencies = {
-    load("Macaroni", "Boost-headers", "1.58"):Target("lib"),
-    load("Macaroni", "CppStd", "2003"):Target("lib"),
-    load("Macaroni", "Boost-filesystem", "1.58"):Target("lib"),
-  },
-};
+function generate()
+  -- Generate C++ code like normal.
+  local cpp = plugins:Get("Cpp")
+  cpp:Run("Generate",
+          { projectVersion=project, path=filePath("target"), output=output})
 
+  -- Use all the CMake generators as an example
+  local root = Path.New(getWorkingDirectory());
+  cmfw.ListsTxt(
+      project,
+      root:NewPathForceSlash("target/CMakeLists.txt"))
+    cmfw.LibInfo(
+      lib,
+      root:NewPathForceSlash("target/macaroni.cmake"))
+    cmfw.ProjectInfo(
+      project,
+      root:NewPathForceSlash("target/pmacaroni.cmake"))
+end
